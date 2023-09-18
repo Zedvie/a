7cb14e4e8c48cb1a9e87cd1d303995425e43f950
Zedvien Daniel Elias Vasquez Martinez <danielvas2005@gmail.com>
2023-09-18 10:40:28 -0500

    creación de Exampol1.py
    Se crea ejemplo de números aleatorios en Python

---
+++ Exampol1.py/Example1.py
@@ -0,0 +1,18 @@
+"""
+Fecha : 18/09/2023
+Autor : Zedvien Daniel Elias Vasquez Martinez
+Objetivo : 
+"""
+import random
+
+random_number = random.randint (1,10)
+print(random_number)
+
+for i in range(0,10):
+    random_number = random.randrange (20, 10, 5)
+    print(random_number)
+
+print("---------------------------------------------------------")
+
+for i in range(0,10):
+    random_number
